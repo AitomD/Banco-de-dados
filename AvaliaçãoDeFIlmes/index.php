c5f1213263b6554da1be7f30ebe8513ca0b04a55
FernandoConsolinRosa <128821668+FernandoConsolinRosa11@users.noreply.github.com>
2025-08-06 22:10:34 -0300

    Add files via upload

---
+++ AvaliaçãoDeFIlmes/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blue Star</title>
+    <link rel="shortcut icon" href="starlogo.png" type="image/x-icon">
+    <link rel="stylesheet" href="estilo/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid mx-5">
+            <a class="navbar-brand " href="#"><img src="img/bluestarlogo.png" alt="logo" class="me-4"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item  mx-2">
+                        <a class="nav-link" aria-current="page" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item  mx-2">
+                        <a class="nav-link" aria-current="page" href="#">SÉRIES</a>
+                    </li>
+                    <li class="nav-item  mx-2">
+                        <a class="nav-link" aria-current="page" href="#">FILMES</a>
+                    </li>
+                </ul>
+                <form class="d-flex mx-5" role="search">
+                    <input class="form-control me-2 w-100" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-primary" type="submit">Search</button>
+                </form>
+                <button type="button" class=" btn btn-primary mx-2 text-end">CADASTRAR</button>
+                <button type="button" class=" btn btn-primary mx-2 text-end">LOGAR</button>
+            </div>
+        </div>
+    </nav>
+
+
+    <footer>
+
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
