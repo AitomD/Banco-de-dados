44d3ccd27d2e0d652cfdaa5ba0641d9d7c8198d9
AitomD <aitomdonatoni@gmail.com>
2025-08-13 19:35:53 -0300

    tootip icone

--- AvaliaçãoDeFIlmes/index.php
+++ AvaliaçãoDeFIlmes/index.php
@@ -47,7 +47,7 @@
             <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
                 <ul class="navbar-nav d-flex align-items-center gap-2">
                     <li class="nav-item mx-2">
-                        <a href="#"><button class="neon-btn">HOME</button></a>
+                        <a href="pages/home.php"><button class="neon-btn">HOME</button></a>
                     </li>
                     <li class="nav-item mx-2">
                         <a href="#"><button class="neon-btn">SUPORTE</button></a>
@@ -123,14 +123,14 @@
                         <div class="d-inline-flex align-items-center">
                             <p class="mb-1 me-3">Aitom Henrique Donatoni </p>
                             <a href="#" class="text-light fs-5 me-3"><i class="fab fa-instagram" data-bs-toggle="tooltip" title="Instagram"></i></i></a>
-                            <a href="#" class="text-light fs-5 me-3"><i class="fab fa-github"></i></a>
+                            <a href="#" class="text-light fs-5 me-3"><i class="fab fa-github" data-bs-toggle="tooltip" title="GitHub"></i></a>
                         </div>
                     </div>
                     <div class="row">
                         <div class="d-inline-flex align-items-center">
                             <p class="mb-1 me-3">Fernando Consolin Rosa</p>
-                            <a href="#" class="text-light fs-5 me-3"><i class="fab fa-instagram"></i></i></a>
-                            <a href="#" class="text-light fs-5 me-3"><i class="fab fa-github"></i></a>
+                            <a href="#" class="text-light fs-5 me-3"><i class="fab fa-instagram" data-bs-toggle="tooltip" title="Instagram"></i></i></a>
+                            <a href="#" class="text-light fs-5 me-3"><i class="fab fa-github" data-bs-toggle="tooltip" title="GitHub"></i></a>
                         </div>
                     </div>
                 </div>
@@ -148,6 +148,14 @@
             </div>
         </div>
     </footer>
+
+<!-- Script para tooltip dos icones footer -->
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  tooltipTriggerList.forEach(el => new bootstrap.Tooltip(el));
+});
+</script>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
