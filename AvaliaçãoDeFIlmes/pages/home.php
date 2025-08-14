add265a108b1b5359f67a221cb28f1bccce3f8dc
AitomD <aitomdonatoni@gmail.com>
2025-08-13 22:36:36 -0300

    banner home

--- AvaliaçãoDeFIlmes/pages/home.php
+++ AvaliaçãoDeFIlmes/pages/home.php
@@ -12,18 +12,12 @@
 <body>
     
 <div class="banner">
-  <h2>teste</h2>
-  <p> teste 2</p>
-  <h5> feijao</h5>
+  <div class="central-card">
+    
+  </div>
 </div>
 
 <div style="height: 45px;"></div>
-
-<div class="banner">
-  <h2>teste</h2>
-  <p> teste 2</p>
-  <h5> feijao</h5>
-</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
